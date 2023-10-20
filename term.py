551ee80d52bf3988379f310fd2e5e87622c6b172
lehrmanaidins <142857908+lehrmanaidins@users.noreply.github.com>
2023-10-20 09:33:53 -0400

    Update term.py

--- term.py
+++ term.py
@@ -6,17 +6,15 @@
 from __future__ import anotations
 
 class Term:
-  def __init__(self, coefficient, exponent) -> None:
-    self.coefficient: float = coefficient
-    self.exponent: float = exponent
+	def __init__(self, coefficient, exponent) -> None:
+		self.coefficient: float = coefficient
+		self.exponent: float = exponent
 
-  def eval_at(x: float) -> float:
-    return self.coefficeint * (x ** self.exponent)
+	def eval_at(x: float) -> float:
+		return self.coefficeint * (x ** self.exponent)
 
-  def as_lambda(self) -> lambda:
-    return lambda x: self.eval_at(x)
+	def as_lambda(self) -> lambda:
+    	return lambda x: self.eval_at(x)
 
-  def __add__(a: Term: b: Term):
-    
-
-x: Term = Term(1, 1)
+  	def __add__(a: Term: b: Term):
+    	pass
